8ae49988f0f0fd0a6c9efe42099a26a568e53b69
Rodney <rodney.marin@gmail.com>
2026-08-27 19:21:08 -0400

    chore: scaffold Android project with Compose, Hilt, Room setup

---
+++ app/src/main/java/com/easypocket/mobile/EasyPocketApp.kt
@@ -0,0 +1,7 @@
+package com.easypocket.mobile
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EasyPocketApp : Application()
---
+++ app/src/main/java/com/easypocket/mobile/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.easypocket.mobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("EasyPocket")
+            }
+        }
+    }
+}
