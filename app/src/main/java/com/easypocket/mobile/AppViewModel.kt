4c139463585edfe7a74acb344d12d8ba07b4ca6f
Rodney <rodney.marin@gmail.com>
2026-08-29 02:34:40 -0400

    chore (temporal): seed de historial fake para probar el gráfico (revertir antes del merge)

--- app/src/main/java/com/easypocket/mobile/AppViewModel.kt
+++ app/src/main/java/com/easypocket/mobile/AppViewModel.kt
@@ -44,6 +44,7 @@ class AppViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 seeder.seedIfEmpty()
+                seeder.seedFakeHistory()
                 _isReady.value = true
             } catch (t: Throwable) { _fatalError.value = t }
         }
