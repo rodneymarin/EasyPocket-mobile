4abb0b71fbe5c1c113f36b3448ffaecc0ccb77a7
Rodney <rodney.marin@gmail.com>
2026-08-30 02:03:31 -0400

    fix: importa clip en AppHeader

--- app/src/main/java/com/easypocket/mobile/ui/components/AppHeader.kt
+++ app/src/main/java/com/easypocket/mobile/ui/components/AppHeader.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
